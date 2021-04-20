da3e49fbe092e4c4fbe08abc50174489d49d6470
catpea <corporate.miou@gmail.com>
2021-04-20 13:20:12 -0400

    New Release

--- index.js
+++ index.js
@@ -1,14 +1,54 @@
 #!/usr/bin/env -S node
 
-import db from './inxed.js';
+import path from 'path';
+import http from 'http';
+import https from 'https';
+import Koa from 'koa';
+import peacoat from 'peacoat';
 
-async function main(){
-  let hello;
-  if(!(await db.has('hello'))){
-    hello = await db.create({ id: 'hello', email: 'user@example.com' });
-  }else{
-    hello = await db.get('hello');
-  }
-  console.log(hello);
+import koaViews from 'koa-views';
+import koaBody from 'koa-body';
+import koaRouter from '@koa/router';
+
+const router = koaRouter();
+
+const render = (koaViews('views'), {map: { html: 'ejs' }});
+router
+  .get('/', home)
+  .get('/view/:id', view)
+  .get('/edit/:id', edit)
+  .get('/history/:id', history)
+  .post('/save', save);
+
+
+app.use(koaBody());
+app.use(router.routes());
+
+async function list(ctx) {
+  const id = ctx.params.id;
+  await ctx.render('list', { posts: posts });
 }
-main();
+// async function main(){
+//   // let hello;
+//   // if(!(await db.has('hello'))){
+//   //   hello = await db.create({ id: 'hello', email: 'user@example.com' });
+//   // }else{
+//   //   hello = await db.get('hello');
+//   // }
+//   // console.log(hello);
+// }
+// main();
+
+
+const app = new Koa();
+app.context.peacoat = peacoat;
+app.keys = ['im a newer secret', 'i like turtle'];
+
+app.use(async ctx => {
+  //ctx.peacoat...
+  ctx.cookies.set('name', 'tobi', { signed: true });
+  ctx.body = 'Hello World';
+});
+
+http.createServer(app.callback()).listen(3000);
+https.createServer(app.callback()).listen(3001);
